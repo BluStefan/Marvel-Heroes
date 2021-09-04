d6bf71b5d0241355b87f9ab0c2bb080234dd734d
BluStefan <s.bozanovic23@gmail.com>
2021-09-04 11:39:42 +0200

    task-3 added heroService

--- marvel-heroes-project/src/components/MainPage/MainPage.jsx
+++ marvel-heroes-project/src/components/MainPage/MainPage.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Hero } from '../Hero/Hero';
 import './MainPage.css';
+import { HeroService } from "../../services/heroService";
 
 export const MainPage = () => {
+    const [heroes, setHeroes] = useState([]);
+    useEffect(() => {
+        HeroService().then((heroes) => {
+            setHeroes(heroes)
+        })
+    },[setHeroes]);
+
     return(
         <div className="mainClass">
         {
-        <Hero />
+        heroes.map(hero => <Hero hero={hero}/>)
 
 
         }
---
+++ marvel-heroes-project/src/services/heroService.js
@@ -0,0 +1,17 @@
+const dataUrl = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=b88668d3d955b6aa2ada65acc5e8cf0f&hash=d609d164ba4b097ea9a25747e96879ad";
+
+export const HeroService = () => {
+    return fetch (dataUrl)
+    .then(response =>{
+        return response.json()
+    })
+    .then(heroes => {
+        return heroes.data.results.map(hero => {
+            return {
+                image:`${hero.thumbnail.path}.${hero.thumbnail.extension}`,
+                name: hero.name,
+                description: hero.description
+            }
+        })
+    })
+}
