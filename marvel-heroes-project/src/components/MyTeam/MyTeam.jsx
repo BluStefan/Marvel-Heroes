a24aec8018d3a1470458b26fccefcfaa7838cea7
BluStefan <s.bozanovic23@gmail.com>
2021-09-04 13:05:41 +0200

    task-5 added myTeam

---
+++ marvel-heroes-project/src/components/MyTeam/MyTeam.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './MyTeam.css';
+
+export const MyTeam = () => {
+    
+    return (
+    <div className="myTeam">
+        <h4>My Team</h4>
+    <div className="someHero">
+        <img className="card-img-left" src="" alt="slika " />
+        <h5 className="card-title">Some Hero</h5>
+    </div>
+</div>
+    )
+}
